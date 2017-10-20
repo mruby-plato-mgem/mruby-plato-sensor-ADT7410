08bf27b67a74e0c3d5387b41c8d2a241eefb892f
窪山 翔士 <Shouji_Kuboyama@scsk-kyushu.co.jp>
2017-10-20 13:58:21 +0900

    change read byte

--- mrblib/adt7410.rb
+++ mrblib/adt7410.rb
@@ -4,7 +4,6 @@
 module PlatoDevice
   class ADT7410 < Plato::Sensor
     I2CADDR = 0x48
-    REG_TEMP = 0
 
     def initialize(addr=I2CADDR, ndigits=3, unit=:C)
       @i2c = Plato::I2C.open(addr)
@@ -14,15 +13,15 @@ module PlatoDevice
 
     def temperature(unit=nil)
         uint = @unit unless unit
-        tmpary = @i2c.read(0x00, 1)
-        tmpary2 = @i2c.read(0x01, 1)
-        t = (tmpary[0] << 8) | tmpary2[0]
-        t= (t >> 3)
+  
+        tmpary = @i2c.read(0x00, 2)
+        t = (tmpary[0] << 8) | tmpary[1]
+        t = (t >> 3)
         if t >= (0x1000) then
             t=t-8192
         end
-        t=t/16
-        t = (t*1.8)+32 if unit == :F   # Celsius temp. -> Fahrenheit temp.
+        t = t / 16.0
+        t = (t*1.8)+32.0 if unit == :F   # Celsius temp. -> Fahrenheit temp.
         t.round(@ndig)
     end
 
